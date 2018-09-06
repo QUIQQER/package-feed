9cf0d03902a78c14ac79de5533d959c768be08dc
Henning Leutz <leutz@pcsg.de>
2018-09-06 13:19:23 +0200

    fix: quiqqer/package-feed#9

--- src/QUI/Feed/Handler/GoogleSitemap/Feed.php
+++ src/QUI/Feed/Handler/GoogleSitemap/Feed.php
@@ -8,6 +8,7 @@ namespace QUI\Feed\Handler\GoogleSitemap;
 
 use QUI\Feed\Handler\AbstractFeed;
 use QUI\Feed\Utils\SimpleXML;
+use QUI\System\Log;
 
 /**
  * Class Feed
@@ -17,12 +18,18 @@ use QUI\Feed\Utils\SimpleXML;
  */
 class Feed extends AbstractFeed
 {
+    /**
+     * @var int
+     */
     protected $pageSize = 0;
-    protected $page = 0;
-
 
     /**
-     * Creat a channel
+     * @var int
+     */
+    protected $page = 0;
+
+    /**
+     * Create a channel
      *
      * @return Channel
      */
@@ -42,8 +49,7 @@ class Feed extends AbstractFeed
      */
     public function getXML()
     {
-
-        $Items = array();
+        $Items = [];
         /** @var Channel[] $Channels */
         $Channels = $this->getChannels();
 
@@ -53,17 +59,16 @@ class Feed extends AbstractFeed
 
             $Items = array_merge($ChannelItems, $Items);
         }
-        
-        
+
         if ($this->pageSize == 0) {
             return $this->createSitemapXML($Items);
         }
-
 
         // Pagination - index
 
         // The link attribute ends on .rss - we need to strip that
         $baseURL = $Channels[0]->getAttribute("link");
+
         if (substr($baseURL, -4) == ".rss") {
             $baseURL = substr($baseURL, 0, -4);
         }
@@ -79,18 +84,15 @@ class Feed extends AbstractFeed
 
         # Check if the site can exist
         if ($this->page < 0 || $this->page > $pageCount) {
-            return $this->createSitemapXML(array());
+            return $this->createSitemapXML([]);
         }
 
         // Pagination - page
         $startIndex = ($this->page - 1) * $this->pageSize;
-       
-        $pageItems = array_slice($Items, $startIndex, $this->pageSize);
+        $pageItems  = array_slice($Items, $startIndex, $this->pageSize);
 
         return $this->createSitemapXML($pageItems);
-
     }
-
 
     /**
      * @param Item[] $items
@@ -112,7 +114,7 @@ class Feed extends AbstractFeed
 
             $ItemXml->addChild(
                 'lastmod',
-                date(\DateTime::ATOM, (int)$Item->getAttribute('date'))
+                date(\DateTime::ATOM, (int)$Item->getAttribute('e_date'))
             );
         }
 
@@ -130,9 +132,9 @@ class Feed extends AbstractFeed
         $XML = new SimpleXML(
             '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" />'
         );
-        
+
         for ($i = 1; $i <= $pages; $i++) {
-            $sitemapURL = $baseURL . "-" . $i . ".xml";
+            $sitemapURL = $baseURL."-".$i.".xml";
             $SitemapXML = $XML->addChild("sitemap");
             $SitemapXML->addChild("loc", $sitemapURL);
         }
