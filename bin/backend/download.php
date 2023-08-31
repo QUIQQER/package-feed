87206043a6b1a359678b81613d61408c216ae50c
Henning Leutz <leutz@pcsg.de>
2023-08-31 06:26:57 +0200

    refactor: psr12 + code style

--- bin/backend/download.php
+++ bin/backend/download.php
@@ -15,7 +15,7 @@ if (!isset($_REQUEST['id'])) {
     $exit(400);
 }
 
-require_once dirname(__FILE__, 5).'/header.php';
+require_once dirname(__FILE__, 5) . '/header.php';
 
 use QUI\Feed\Manager as FeedManager;
 
@@ -29,19 +29,19 @@ $Request = QUI::getRequest();
 
 try {
     $FeedManager = new FeedManager();
-    $Feed        = $FeedManager->getFeed((int)$_REQUEST['id']);
+    $Feed = $FeedManager->getFeed((int)$_REQUEST['id']);
 
     if (!$FeedManager->isFeedBuilt($Feed) && !$Feed->getAttribute('directOutput')) {
         $exit(503);
     }
 
     $xmlString = $FeedManager->getFeedOutput($Feed);
-    $varDir    = QUI::getPackage('quiqqer/feed')->getVarDir();
-    $tmpFile   = $varDir.\hash('sha256', \microtime(true)).'.xml';
+    $varDir = QUI::getPackage('quiqqer/feed')->getVarDir();
+    $tmpFile = $varDir . hash('sha256', microtime(true)) . '.xml';
 
-    \file_put_contents($tmpFile, $xmlString);
+    file_put_contents($tmpFile, $xmlString);
 
-    QUI\Utils\System\File::send($tmpFile, 0, 'feed_'.$Feed->getId().'.xml');
+    QUI\Utils\System\File::send($tmpFile, 0, 'feed_' . $Feed->getId() . '.xml');
 } catch (\Exception $Exception) {
     QUI\System\Log::addDebug($Exception->getMessage());
     $exit(500);
